c63128af448fe9787b2ba2a98b3cd2f1cc40ef91
zareex <63774713+re4dev@users.noreply.github.com>
2023-04-15 21:56:33 +0200

    Add navigation

---
+++ client/fisch-quiz/src/components/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Button, Link, Spacer } from "@nextui-org/react";
+
+function Navigation() {
+  return (
+    <Navbar isBordered>
+        <Navbar.Content hideIn="xs">
+            <Navbar.Link href="#">Alle</Navbar.Link>
+            <Spacer x={0.5} />
+            <Navbar.Link href="#">Favoriten</Navbar.Link>
+            <Spacer x={0.5} />
+            <Navbar.Link href="#">Falsche</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+            <Navbar.Link color="inherit" href="#">
+                Login
+            </Navbar.Link>
+            <Navbar.Item>
+                <Button auto flat as={Link} href="#">
+                Sign Up
+                </Button>
+            </Navbar.Item>
+        </Navbar.Content>
+    </Navbar>
+  )
+}
+
+export default Navigation
