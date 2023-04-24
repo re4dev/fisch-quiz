7330e01f9763f504b1c18ce74f763c22decb0e67
zareex <63774713+re4dev@users.noreply.github.com>
2023-04-24 20:00:54 +0200

    Fix import

--- client/fisch-quiz/src/pages/index.tsx
+++ client/fisch-quiz/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Footer from '@/components/Footer'
-import Quiz from '@/components/quiz'
+import Quiz from '@/components/Quiz'
 
 const inter = Inter({ subsets: ['latin'] })
 
