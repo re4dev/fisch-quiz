3750adcfce0cf7ba2f696ccd01ff68f1f2a07273
rene <rene.viermann@outlook.de>
2023-08-02 23:47:51 +0200

    feat(frontend): Modify _app.tsx to provide session

--- client/fisch-quiz/src/pages/_app.tsx
+++ client/fisch-quiz/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react';
-import Footer from '@/components/Footer'
+import Footer from '@/components/Footer';
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }: AppProps) {
+import type { Session } from "next-auth"
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{session: Session}>) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-      <Footer />
-    </NextUIProvider>
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+        <Footer />
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
