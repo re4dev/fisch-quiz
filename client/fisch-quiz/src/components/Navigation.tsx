62917d0315e87c2fb6c3718f38b7efd3184c059b
rene <rene.viermann@outlook.de>
2023-08-02 23:48:45 +0200

    feat(frontend): Add button for signin / signout

--- client/fisch-quiz/src/components/Navigation.tsx
+++ client/fisch-quiz/src/components/Navigation.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Navbar, Button, Link, Spacer } from "@nextui-org/react";
+import { useSession, signIn, signOut  } from 'next-auth/react';
 
 function Navigation() {
+    const { data: session } = useSession();
   return (
     <Navbar isBordered>
         <Navbar.Content hideIn="xs">
@@ -12,14 +14,24 @@ function Navigation() {
             <Navbar.Link href="#">Falsche</Navbar.Link>
         </Navbar.Content>
         <Navbar.Content>
-            <Navbar.Link color="inherit" href="/auth/login">
+            {/* <Navbar.Link color="inherit" href="/auth/login">
                 Login
             </Navbar.Link>
             <Navbar.Item>
                 <Button auto flat as={Link} href="/auth/registrieren">
                     Sign Up
                 </Button>
-            </Navbar.Item>
+            </Navbar.Item> */}
+            { session ? (
+                <>
+                    <p>{session.user.name}, {session.user.email}</p>
+                    <Button onPress={() => signOut()}>Sign Out</Button>
+                </>
+            ):
+            (
+                <Button onPress={() => signIn()}>Sign In</Button>
+            )
+            }
         </Navbar.Content>
     </Navbar>
   )
