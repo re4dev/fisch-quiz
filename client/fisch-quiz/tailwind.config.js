11591224ca7d9447b79aab5787ea2dc4745df899
re4dev <rene.viermann@outlook.de>
2023-10-25 12:44:04 +0200

    style(feat): Add styles for tailwindcss

--- client/fisch-quiz/tailwind.config.js
+++ client/fisch-quiz/tailwind.config.js
@@ -11,7 +11,24 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkColor: '#1E2022',
+        mainColor: '#C9D6DF',
+        textColor: '#F0F5F9',
+        bgColor: '#52616B',
+        bgBlueColor: '#D2E0FB',
+      },
+      width: {
+        '400px': '400px',
+        '450px': '450px',
+        '500px': '500px',
+        '550px': '550px',
+        '600px': '600px',
+        '650px': '650px',
+      }
+    },
+
   },
   darkMode: "class",
   plugins: [nextui()]
