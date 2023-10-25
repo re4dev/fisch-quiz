bfdd0cdf0c97b3e32c8014ef8baa45691d35ad91
re4dev <rene.viermann@outlook.de>
2023-10-25 12:44:29 +0200

    feat(frontend): Update footer

--- client/fisch-quiz/src/components/Footer.tsx
+++ client/fisch-quiz/src/components/Footer.tsx
@@ -4,26 +4,22 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="mt-auto justify-center w-fit mx-auto">
-        {/* <Grid.Container gap={1} justify="center">
-            <Grid>
-                <Link href="https://github.com/re4dev/fisch-quiz" target="_blank">Github</Link>
-            </Grid>
-            <Spacer x={1} />
-            <Grid>
-                <Link href="#">Impressum</Link>
-            </Grid>
-            <Spacer x={1} />
-            <Grid>
-                <Link href="#">Datenschutz</Link>
-            </Grid>
-            <Spacer x={1} />
-            <Grid>
-                <Link href="/contact">Kontakt</Link>
-            </Grid>
-        </Grid.Container> */}
-        <div>
-                <p>© 2023 Fisch-Quiz. All Rights Reserved.</p>
+    <footer className="mt-auto">
+        <div className="bg-darkColor text-center">
+            <div className="flex justify-center">
+                <div className="px-10 text-textColor">
+                    <Link href="https://github.com/re4dev/fisch-quiz" target="_blank">Github</Link>
+                </div>
+                <div className="pr-10 text-textColor">
+                    <Link href="/contact">Kontakt</Link>
+                </div>
+                <div className="pr-10 text-textColor">
+                    <Link href="/">Changelog</Link>
+                </div>
+            </div>
+            <div className="px-10 text-textColor">
+                    <p>© 2023 Fisch-Quiz. All Rights Reserved.</p>
+            </div>
         </div>
     </footer>
  );
