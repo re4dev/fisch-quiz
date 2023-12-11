41585d8e21818551e23c909ba4163b63fca3b523
re4dev <rene.viermann@outlook.de>
2023-12-11 19:45:06 +0100

    fix(frontend): missing var

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone"
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6LcenSwpAAAAAGjwkgzj1dfhh3LaT1tx1XiyVHUm'
+  }
 }
 
 module.exports = nextConfig
