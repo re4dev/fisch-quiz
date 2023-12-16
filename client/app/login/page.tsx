8a02036c57713933082b5a8d0d26ad376430ffe7
re4dev <rene.viermann@outlook.de>
2023-12-16 01:39:57 +0100

    feat(frontend): Update login

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Card,
   Button,
   Input,
+  Link,
 } from '@nextui-org/react';
 import { User, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
@@ -53,41 +53,35 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <div className='justify-center flex content-center'>
-        <Card >
-          <p className=' text-base font-bold mb-20px'>
-            Anmelden
-          </p>
+    <div className='p-6 h-64 w-96 mx-auto mt-20'>
+      <div className='bg-gray-100 p-8 rounded-lg shadow-md w-96'>
           <Input
             type='email'
             variant="bordered"
-            color="primary"
             size="lg"
-            placeholder="Benutzername"
+            placeholder="E-Mail"
             value={email}
             onValueChange={setEmail}
+            className='mb-3'
           />
           <Input
             variant="bordered"
             type='password'
-            color="primary"
             size="lg"
             placeholder="Passwort"
             value={password}
             onValueChange={setPassword}
+            className=''
           />
 
-          <Button onClick={handleSignIn}>Einloggen</Button>
-          <Button onClick={handleSignUp}>Registrieren</Button>
+          <Button onClick={handleSignIn} className='w-auto mx-auto mt-2 mb-4'>Einloggen</Button>
 
-           {/* <p>Neu hier?</p>
-          <div className='justify-center flex content-center'>
-            <Link href="/auth/registrieren" className="font-medium text-indigo-600 hover:text-indigo-500">
+          <p className='p-0 m-0 text-black'>Neu hier?</p>
+          <div className=''>
+            <Link href="/register" className="p-0 m-0 text-xs align-top text-black">
               Registrieren
             </Link>
-          </div> */}
-        </Card>
+          </div>
       </div>
     </div>
 
