9877eaba6f27bde5e1605141dfe66213ae15e3d7
re4dev <rene.viermann@outlook.de>
2023-12-21 18:35:44 +0100

    fix(frontend): Update type of request

--- client/app/auth/callback/route.ts
+++ client/app/auth/callback/route.ts
@@ -1,10 +1,11 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: { url: string | URL; }){
+export async function GET(request: NextRequest){
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get('code');
+    
     if (code) {
         const cookieStore = cookies();
         const supabase = createRouteHandlerClient({cookies: () => cookieStore})
